0d2f53dedc89320055d07e38d3c937e7a413d7ae
Feruz <feruzjuraev6@gmail.com>
2022-04-30 06:12:31 +0500

    first commit

---
+++ js/project.js
@@ -0,0 +1,39 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded' , () => {
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+
+const addForm = document.querySelector('form.add'),
+      addInput = addForm.querySelector('adding__input'),
+      spisok = document.querySelectorAll('.promo__interactive-list');
+      addChekbox = addForm.querySelector('[type:"checkbox"]');
+
+
+      const newFilm = addInput.value;
+      const checking = addChekbox.value;
+
+
+
+      function createMovielist(){
+
+
+
+
+      }
+
+
+
+   });
+
+
+   
